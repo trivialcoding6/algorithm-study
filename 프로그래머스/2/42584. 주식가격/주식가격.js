a50d35dc4f432f1537aef41f6420acae3fc372e8
trivialcoding6 <167159243+trivialcoding6@users.noreply.github.com>
2025-01-20 08:50:42 +0900

    [level 2] Title: 주식가격, Time: 30.17 ms, Memory: 43.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,14 +1,23 @@
 function solution(prices) {
   const answer = Array(prices.length).fill(0);
-  for (let i = 0; i < prices.length; i++) {
-    for (let j = i + 1; j < prices.length; j++) {
-        
-      answer[i]++;
+  const stack = [];
 
-      if (prices[i] > prices[j]) {
-        break;
-      }
+  for (let currentTime = 0; currentTime < prices.length; currentTime++) {
+    while (
+      stack.length &&
+      prices[currentTime] < prices[stack[stack.length - 1]]
+    ) {
+      const prevTime = stack.pop();
+      answer[prevTime] = currentTime - prevTime;
     }
+
+    stack.push(currentTime);
+  }
+
+  const lastTime = prices.length - 1;
+  while (stack.length) {
+    const prevTime = stack.pop();
+    answer[prevTime] = lastTime - prevTime;
   }
 
   return answer;
