b7c440d9cfbc6954fd59c104cff7557bf62895a6
trivialcoding6 <167159243+trivialcoding6@users.noreply.github.com>
2025-01-26 12:42:35 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,16 @@
+words = []
+result = ''
+max_length = 0
+
+for _ in range(5):
+    word = input()
+    words.append(word)
+    max_length = max(max_length, len(word))
+
+
+for i in range(max_length):
+    for j in range(5):
+        if i < len(words[j]):
+            result += words[j][i]
+
+print(result)
