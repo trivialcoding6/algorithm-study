a1a54e625c4006400c2d0e3190738bf2a7ff9154
trivialcoding6 <167159243+trivialcoding6@users.noreply.github.com>
2025-02-01 10:01:56 +0900

    [level 2] Title: H-Index, Time: 0.40 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -0,0 +1,11 @@
+function solution(citations) {
+    citations.sort((a, b) => b - a);
+    
+    for (let i = 0; i < citations.length; i++) {
+        if (citations[i] < i + 1) {
+            return i;
+        }
+    }
+    
+    return citations.length;
+}
