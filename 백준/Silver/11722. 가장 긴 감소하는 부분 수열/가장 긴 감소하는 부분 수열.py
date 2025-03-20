56e60a4ed410c809fec18bc1d7a0490aca6fd263
trivialcoding6 <167159243+trivialcoding6@users.noreply.github.com>
2025-03-21 08:10:55 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 68 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
@@ -0,0 +1,14 @@
+def longest_decreasing_subsequence(arr):
+    n = len(arr)
+    dp = [1] * n
+
+    for i in range(1, n):
+        for j in range(i):
+            if arr[j] > arr[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+
+    return max(dp)
+
+n = int(input())
+arr = list(map(int, input().split()))
+print(longest_decreasing_subsequence(arr))
